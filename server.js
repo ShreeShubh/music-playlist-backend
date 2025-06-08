64a13e03bd203b153ee9cd009217174ab65ae664
Shubham Shree <shubhamshree.dev@gmail.com>
2025-06-08 15:04:14 +0530

    cors updated

--- server.js
+++ server.js
@@ -13,24 +13,7 @@ const app = express()
 const PORT = process.env.PORT || 5000
 
 // middlewares
-const allowedOrigins = [
-  "http://localhost:5173",
-  "https://music-playlist-frontend-liart.vercel.app/",
-]
-
-app.use(
-  cors({
-    origin: function (origin, callback) {
-      if (!origin || allowedOrigins.includes(origin)) {
-        callback(null, true)
-      } else {
-        callback(new Error("Not allowed by CORS"))
-      }
-    },
-    credentials: true,
-  })
-)
-
+app.use(cors())
 app.use(express.json())
 
 // app.get("/", (req, res) => {
