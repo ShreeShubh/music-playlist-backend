194618121e1e563b6b07dbfe65bf30f559f68ea9
Shubham Shree <shubhamshree.dev@gmail.com>
2025-06-08 14:26:19 +0530

    cors updated

--- server.js
+++ server.js
@@ -13,12 +13,7 @@ const app = express()
 const PORT = process.env.PORT || 5000
 
 // middlewares
-app.use(
-  cors({
-    origin: "http://localhost:5173",
-    credentials: true,
-  })
-)
+app.use(cors())
 app.use(express.json())
 
 // app.get("/", (req, res) => {
